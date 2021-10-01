abedd06ed77858c15e8f21e4e4c64678c94df957
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-01 14:07:07 +0100

    6th commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-  int i = 0;
-  for (, i < 10, i++)
-    printf("%d", i);
-  printf("\n");
-  return (0);
+int i = 0;
+for (; i < 10; i++)
+printf("%d", i);
+printf("\n");
+return (0);
 }
