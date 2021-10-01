83f315e8d4ea36f4dcb1cbbfc1000dc5b5f2b150
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-01 14:08:36 +0100

    7th commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-  int i = 0;
-  for (, i < 10, i++)
-    putchar(i + '0');
-  putchar('\n');
-  return (0);
+int i = 0;
+for (; i < 10; i++)
+putchar(i + '0');
+putchar('\n');
+return (0);
 }
