3041db710ce64eb06286ddcc7e1557f077dda390
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-01 11:49:46 +0100

    10th commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,13 @@
  */
 int main(void)
 {
-  int i;
-int j;
+int i;
  for (i = 0, i < 10, i++)
-   {
-   for (j = 1, j < 10, j++)
       {
 	putchar(i + '0');
-	putchar(j + '0');
 	putchar(',');
+	putchar(' ');
       }
       putchar('\n');
-  }
   return (0);
 }
