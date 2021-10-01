9f3af585b12b70e16d44444fff45cbc68751c217
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-01 14:11:20 +0100

    9th commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-  char c;
-  int i = 0;
-  for (, i < 10, i++)
-    putchar(i + '0');
-  for (c = 'a', c <= 'f', c++)
-    putchar(c);
-  putchar('\n);
-  return (0);
+char c;
+int i = 0;
+for (; i < 10; i++)
+putchar(i + '0');
+for (c = 'a'; c <= 'f'; c++)
+putchar(c);
+putchar('\n);
+return (0);
 }
