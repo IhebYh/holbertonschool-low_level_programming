20bfc29872d18d3980a475bf400247d9c74b6dc2
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-01 11:15:00 +0100

    9th commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char c;
+  int i = 0;
+  for (, i < 10, i++)
+    putchar(i + '0');
+  for (c = 'a', c <= 'f', c++)
+    putchar(c);
+  putchar('\n);
+  return (0);
+}
