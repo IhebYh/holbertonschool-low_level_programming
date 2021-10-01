204616ff9a6514d4745a75d3111cb9304c403d81
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-01 13:28:22 +0100

    2nd commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,8 +18,7 @@ int main(void)
 	else if ((n % 10) == 0)
 	printf("is %d and is 0", n % 10);
 	else if ((n % 10) < 6)
-	&&
-	(n != 0))
+	&&(n != 0))
 	printf("is %d and is less than 6 and not 0", n % 10);
 	return (0);
 }
