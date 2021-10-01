be5e210783727704529d4595c2207dbf5d2158af
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-01 11:07:47 +0100

    7th commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int i = 0;
+  for (, i < 10, i++)
+    putchar(i + '0');
+  putchar('\n');
+  return (0);
+}
