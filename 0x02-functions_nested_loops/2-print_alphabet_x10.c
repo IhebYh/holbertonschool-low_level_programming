8089a780c36ae5166b642bf57cd4adf015e0a4a7
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-05 13:22:00 +0100

    3rd commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,4 @@
-#include "main.h"
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-print_alphabet_x10();
-return (0);
-}
+#include"main.h"
 /**
  * print_alphabet_x10 - check the code
  *
@@ -20,7 +10,7 @@ char c = 'a';
 int i = 0;
 for (; i < 10; i++)
 {
-for (; c < 'z'; c++)
+for (; c <= 'z'; c++)
 _putchar(c);
 _putchar('\n');
 }
