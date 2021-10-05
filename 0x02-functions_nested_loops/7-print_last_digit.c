4fdfc9e6b688d25cc1e996ec59ea96218488d13c
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-05 14:54:21 +0100

    8th commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,18 +8,13 @@
  */
 int print_last_digit(int n)
 {
-int x = n;
+int x, y;
+x = n % 10;
 if (x < 0)
-x = -1 * x;
-while (x > 10)
 {
-x %= 10;
+x = -x;
 }
-if ((x < 10) && (x != 0))
-{
-_putchar(x + '0');
-return (x);
-}
-_putchar('0');
-return (0);
+y = '0' + x;
+_putchar(y);
+return (y);
 }
