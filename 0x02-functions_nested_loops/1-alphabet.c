c5ad98bd2b5efea69e85dbe61c28137f62fc2209
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-05 12:34:58 +0100

    2nd commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,23 @@
 #include"main.h"
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 print_alphabet();
 return (0);
 }
+
 /**
  * print_alphabet - check the code
  *
  * Return: Always 0.
  */
+
 void print_alphabet(void)
 {
 char c = 'a';
