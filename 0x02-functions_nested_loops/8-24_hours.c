5b3a38d328fe01ba1d51b3554982ed6546d4dc46
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-05 16:39:28 +0100

    9th commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,11 +9,11 @@ void jack_bauer(void)
 int i, j, k, h, l, m;
 for (i = 0; i < 24; i++)
 {
-l = i \ 10;
+l = i / 10;
 m = i % 10;
 for (j = 0; j < 60; j++)
 {
-k = j \ 10;
+k = j / 10;
 h = j % 10;
 _putchar(l + '0');
 _putchar(m + '0');
