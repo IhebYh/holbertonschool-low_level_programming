4c56fa8cf6b6ec2a7dc4ae0bac78661b86f2dfd7
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-05 17:27:56 +0100

    13th commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,22 +6,42 @@
  */
 void print_times_table(int n)
 {
-int i = 0, j = 0, k;
-while ((i <= n) && (j <= n))
+int i, j, k;
+if (n >= 0 && n <= 15)
 {
-k = 0;
-for (i = 0; i < n ; i++)
+for (i = 0; i <= n; i++)
 {
-k += j;
-_putchar(k + '0');
+_putchar('0');
+if (n != 0)
 _putchar(',');
+for (j = 1; j <= n; j++)
+{
+k = i * j;
+if (k < 10)
+{
 _putchar(' ');
 _putchar(' ');
+_putchar(' ');
+_putchar('0' + k);
 }
-_putchar(k + '0');
+else if (k < 100)
+{
 _putchar(' ');
-_putchar(' '); 
-j++;
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+else
+{
+_putchar(' ');
+_putchar((k / 100) + '0');
+_putchar(((k % 100) / 10) + '0');
+_putchar(((k % 100) % 10) + '0');
+}
+if (j < n)
+_putchar(',');
+}
 _putchar('\n');
+}
 }
 }
