272c4492673b085b4503bcdaccf408ce68c3da50
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-05 13:39:31 +0100

    6th commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,57 +1,26 @@
 #include "main.h"
-
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-int r;
-r = print_sign(98);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-r = print_sign(0);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-r = print_sign(0xff);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-r = print_sign(-1);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-return (0);
-}
 /**
  * print_sign - Entry point
  *
- * @x : int
+ * @n : int
  *
  * Return: Always 1 (Success)
  */
-int print_sign(int x)
+int print_sign(int n)
 {
-if (x > 0)
+if (n > 0)
 {
 _putchar('+');
 return (1);
 }
-else if (x < 0)
+else if (n < 0)
 {
 _putchar('-');
 return (-1);
 }
-else if (x == 0)
+else if (n == 0)
 {
-_putchar(x + '0');
+_putchar(n + '0');
 }
 return (0);
 }
