3e7ddec6b1a82a0acd1b30c88e4665f9454ad780
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-06 09:33:58 +0100

    14th commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,7 +7,7 @@
  */
 void print_multiple(void)
 {
-int s3 = 0, s5 = 0;
+int i, s3 = 0, s5 = 0;
 for (i = 0; i < 1024; i++)
 {
 if (i % 3 == 0)
@@ -17,5 +17,4 @@ s5 += i;
 }
 printf("The sum of all the multiples of 5 below 1024: %d \n", s5);
 printf("The sum of all the multiples of 3 below 1024: %d \n", s3);
-return (0);
 }
