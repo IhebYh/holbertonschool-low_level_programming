01b0b340ca9e1115222cb36568c4c146c90aa8fa
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-06 10:52:40 +0100

    16th commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+  long int f = 3, i = 0, f1 = 1, f2 = 0, fe = 0;
+while (fe + f < 4000000)
+{
+if (f % 2 == 0)
+fe += f;
+f = f1 + f2;
+f2 = f1;
+f1 = f;
+i++;
+}
+printf("%d\n", fe);
+return (0);
