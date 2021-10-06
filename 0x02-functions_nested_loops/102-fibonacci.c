59b86daf46a824885c0f0c82864093cc278a7ffa
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-06 10:32:25 +0100

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 /**
- * times_table - Entry point
+ * main - Entry point
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-long int f= 3, i = 0, f1 = 1, f2 = 0;
-while(i < 50)
+long int f = 3, i = 0, f1 = 1, f2 = 0;
+while (i < 50)
 {
 f = f1 + f2;
 f2 = f1;
 f1 = f;
 if (i == 49)
-printf("%ld ", f);   
+printf("%ld ", f);
+else
 printf("%ld, ", f);
 i++;
 }
