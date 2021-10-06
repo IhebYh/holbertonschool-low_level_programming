53bbc4789e68e235ef5de592a1996033535a89b3
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-06 11:16:29 +0100

    17th commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+long int f = 3, i = 0, f1 = 1, f2 = 0, f1_1, f1_0, f2_1, f2_0, f_1, f_0;
+while (i < 89)
+{
+f = f1 + f2;
+f2 = f1;
+f1 = f;
+printf("%ld, ", f);
+i++;
+}
+f_1 = f / 1000000000;
+f_0 = f % 1000000000;
+f1_1 = f1 / 1000000000;
+f1_0 = f1 % 1000000000;
+f2_1 = f2 / 1000000000;
+f2_0 = f2 % 1000000000;
+while (i < 98)
+{
+f_1 = f1_1 + f2_1;
+f_0 = f1_0 + f2_0;
+f2_1 = f1_1;
+f2_0 = f1_0;
+f1_1 = f_1;
+f1_0 = f_0;
+if (i == 97)
+printf("%ld%ld", f_1, f_0);
+else
+printf("%ld%ld, ", f_1, f_0);
+i++;
+}
+printf("\n");
+return (0);
+}
