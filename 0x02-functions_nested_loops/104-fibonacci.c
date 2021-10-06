61bd24c91c067aa5b4c0572e163bda814783a5a2
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-06 11:22:34 +0100

    17th commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 long int f = 3, i = 0, f1 = 1, f2 = 0, f1_1, f1_0, f2_1, f2_0, f_1, f_0;
-while (i < 89)
+/*while (i < 89)
 {
 f = f1 + f2;
 f2 = f1;
@@ -34,6 +34,6 @@ printf("%ld%ld\n", f_1, f_0);
 else
 printf("%ld%ld, ", f_1, f_0);
 i++;
-}
+}*/
 return (0);
 }
