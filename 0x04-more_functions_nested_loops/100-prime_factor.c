58e7d965bc9597a61393b01859afec1e8f25577b
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-07 13:35:41 +0100

    12th commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 1 or 0
+ */
+
+int main(void)
+{
+long int i, j, n = 612852475143, isPrime;
+
+for (i = 2; i <= n; i++)
+{
+if (n % i == 0)
+{
+isPrime = 1;
+for (j = 2; j <= i / 2; j++)
+{
+if (i % j == 0)
+isPrime = 0;
+if (isPrime == 0)
+printf("%d, ", n);
+}
+}
+}
+return (0);
+}
