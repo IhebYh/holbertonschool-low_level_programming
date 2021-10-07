6697cdadc3e254ac3644b9ed248100d9824edc2a
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-07 12:48:57 +0100

    8th commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,6 +13,7 @@ void print_line(int n)
 int i, j;
 
 if (n > 0)
+{
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < i; j++)
@@ -22,4 +23,6 @@ _putchar(' ');
 _putchar('\\');
 _putchar('\n');
 }
+else
+_putchar('\n');
 }
