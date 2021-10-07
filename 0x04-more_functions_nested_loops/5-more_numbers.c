53c29fd5eee53a50726bc4ee04140724ec4fc791
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-07 10:15:33 +0100

    6th commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers from 0 to 14, 10 times
+ *
+ * Return: 1 or 0
+ */
+
+void more_numbers(void)
+{
+int i;
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i < 15; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+}
+}
