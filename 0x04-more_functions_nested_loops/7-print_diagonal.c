cd783222fefcae06971b3fbf48b1a9e19c0509a4
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-07 10:31:23 +0100

    8th commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - print numbers
+ *
+ *@n : int
+ *
+ * Return: 1 or 0
+ */
+
+void print_line(int n)
+{
+int i, j;
+
+if (n > 0)
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+_putchar('\n');
+}
