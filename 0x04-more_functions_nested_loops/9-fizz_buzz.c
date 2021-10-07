f9a04661b227858ee6f1ec0bc007850e99d43bd6
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-07 12:59:13 +0100

    10th commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (((i % 3) + (i % 5)) == 0)
+if (((i % 3) == 0) && ((i % 5) == 0))
 printf("FizzBuzz");
 else if (i % 3 == 0)
 printf("Fizz");
@@ -19,7 +19,7 @@ else if (i % 5 == 0)
 printf("Buzz");
 else
 printf("%d", i);
-if(i < 100)
+if (i < 100)
 printf(" ");
 else
 printf("\n");
