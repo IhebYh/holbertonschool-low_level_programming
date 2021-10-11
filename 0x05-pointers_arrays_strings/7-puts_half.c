918cb834e79f8f10e6ac3584165f3692f12fe065
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-11 12:56:41 +0100

    8th commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,14 +9,12 @@ void puts_half(char *str)
 {
 int i = 0, n = 0;
 while (str[i++])
-{
 n++;
-}
-if (n % 2 == 0)
+if (n % 2)
 i = n / 2;
 else
 i = (n - 1) / 2;
-for (; i < n; i++)
+for (; i =< n; i++)
 {
 _putchar(*(str + i));
 }
