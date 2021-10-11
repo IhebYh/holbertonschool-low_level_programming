0fbbda6e56f78f18d281b972d3d8cd8ec67005ba
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-11 12:35:39 +0100

    7th commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _puts2 - entry point
+ * puts2 - entry point
  * @str :char
  * Return:int
  */
-void _puts2(char *str)
+void puts2(char *str)
 {
 int i;
 for (i = 0; str[i] != '\0'; i += 2)
