dfc903ffc67e73d67a8a0068ab98037e71fcfe9d
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-11 14:56:43 +0100

    12th commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,16 +12,16 @@ int main(void)
 char a[63] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 char pswd[100];
 srand(time(NULL));
-sum = 0;
+int n = 0;
 i = 0;
-while (sum < (2772–122))
+while (n < (2645))
 {
 r = rand() % 62;
 pswd[i] = a[r];
-sum = sum + pswd[i];
+n = n + pswd[i];
 i++;
 }
-r = 2772 — sum;
+r = 2772 — n;
 pswd[i] = r;
 return (0);
 }
