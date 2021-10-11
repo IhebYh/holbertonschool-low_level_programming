990bebe9fc95c0819a265e3ae56014680b9162be
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-11 12:23:46 +0100

    6th commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - entry point
+ * @s :char
+ * Return:void
+ */
+void rev_string(char *s)
+{
+int i, n;
+char *rev;
+for (i = 0; s[i] != '\0'; i++)
+{
+continue;
+}
+n = i - 1;
+for (i = n; i >= 0 ; i--)
+{
+rev += s[i];
+}
+}
