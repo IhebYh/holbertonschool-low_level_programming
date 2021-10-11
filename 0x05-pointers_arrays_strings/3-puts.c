d2b3cee15a72c7a54bd9dd198226c6b5a630d0de
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-11 12:13:00 +0100

    4th commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - entry point
+ * @str :char
+ * Return:int
+ */
+void _puts(char *str)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
