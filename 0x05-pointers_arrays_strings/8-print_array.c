bcdb78c76f36ea64d509fc257dff0bbb5bc09a5c
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-11 13:21:38 +0100

    9th commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,11 +9,12 @@
 void print_array(int *a, int n)
 {
 int i = 0;
-while(a[i++])
+while(a[i])
 {
 if (i == n - 2)
 break;
 printf("%d, ", *(a + i));
+i++;
 }
-printf("%d\n", *(a + i));
+printf("%d\n", *(a + (n - 1)));
 }
