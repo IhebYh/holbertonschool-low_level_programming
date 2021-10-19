93f6fea30311fc1b6dfa1f96fb6c78cda74187c9
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-19 11:33:42 +0100

    8th commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ *
+ * @a: the row of the array
+ *
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int i = 0;
+while (*a)
+{
+_putchar(*a[i]);
+if (i == 7)
+{
+i = 0;
+_putchar('\n);
+}
+}
+}
