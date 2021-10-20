2e7cfc7b8c673a9ca1611a843ac524900c4fc798
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-20 14:39:43 +0100

    8th commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,14 +7,8 @@
 int is_palindrome(char *s)
 {
 int n = 0;
-if (*s == "")
-return (1);
-else
-while(*s != '\0'){
-s++;
-n++;
-}
-return (isPal(*s, 0, n - 1));
+n = _strlen(s) - 1;
+return (isPal(s, 0, n - 1));
 }
 /**
  * isPal - entry point
@@ -27,8 +21,21 @@ int isPal(char *s, int i, int n)
 {
 if (s[i] != s[n])
 return (0);
-else if ((s[i] == s[n]) && ( i <= n))
-return (isPal(*s, i + 1, n - 1));
-else 
+else if ( i > n)
 return (1);
+else
+return (isPal(s, i + 1, n - 1));
+}
+/**
+ * _strlen - entry point
+ * @s:char
+ * Return:int
+ */
+int _strlen(char *s)
+{
+int i = 0;
+if (*s)
+return (i + _strlen(s + 1));
+else
+return (0);
 }
