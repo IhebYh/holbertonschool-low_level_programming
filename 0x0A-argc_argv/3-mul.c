f29fb900e4c3b514adb198c8fbf7614682521ff2
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-22 10:00:51 +0100

    4th commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc:int
+ * @argv:char[]
+ * Return:int
+ */
+
+int main(int argc, char *argv[])
+{
+int n, m, mul;
+if (argc != 3)
+{
+printf("Error");
+return (1);
+}
+n = atoi(argv[2]);
+m = atoi(argv[3]);
+mul = n * m;
+printf("%d\n",mul);
+return (0);
+}
