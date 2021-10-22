4ceec4e6d9116727868393d0ae5b902baa4f6513
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-22 11:14:56 +0100

    6th commit

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc:int
+ * @argv:char[]
+ * Return:int
+ */
+
+int main(int argc, char *argv[])
+{
+int i, res, n, cents = 0;
+int a[5] = {25, 10, 5, 2, 1};
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+res = atoi(argv[1]);
+if (res < 0)
+printf("0\n");
+else
+for (i = 0; i < 5; i++)
+{
+n = res / a[i];
+cents += n;
+res -= n *a[i];
+}
+printf("%d\n", cents);
+return (0);
+}
