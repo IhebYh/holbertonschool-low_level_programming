187033d9784f2a955b1c10e124e744d7a8dbdee4
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-22 09:52:00 +0100

    2nd commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc:int
+ * @argv:char[]
+ * Return:int
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", arg);
+return (0);
+}
