e617dc7091ca90145c61b83d61c7becb29c82bc2
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-22 13:08:32 +0100

    6th commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,10 +17,10 @@ printf("Error\n");
 return (1);
 }
 res = atoi(argv[1]);
-if (res =< 0)
+if (res < 0)
 printf("0\n");
 else
-for (i = 0; i < 5; i++)
+for (i = 0; i < 5 && res; i++)
 {
 n = res / a[i];
 cents += n;
