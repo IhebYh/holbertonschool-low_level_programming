64fe3af4917ae2c6b7e0571083d74e0235281797
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-26 12:16:18 +0100

    5th commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdlib.h>
+/**
+ * free_grid - entry point
+ * @grid:int
+ * @height:int
+ * Return:Null or pointer
+ */
+void free_grid(int **grid, int height)
+{
+  int i = 0;
+  for (; i < height; i++)
+    free(grid[i]);
+  free(grid);
+}
