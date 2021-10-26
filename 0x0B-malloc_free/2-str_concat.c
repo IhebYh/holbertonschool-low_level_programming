549c9f86e48a406ddae6921b3a9abe8e60583956
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-26 11:23:45 +0100

    3rd commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,8 +10,14 @@ char *str_concat(char *s1, char *s2)
 {
 int i, j;
 char *s;
+if (s1 == NULL)
+i = 0;
+else
 for(i = 0; s1[i]; i++)
 ;
+if (s2 == NULL)
+j = 0;
+else
 for (j = 0; s2[j]; j++)
 ;
 s = malloc((i + j + 1) * sizeof(char));
