05afe8027c613a636c82152651640e04351988e5
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-26 11:20:13 +0100

    3rd commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include<stdlib.h>
+/**
+ * str_concat - entry point
+ * @s1:char
+ * @s2:char
+ * Return:Null or pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+int i, j;
+char *s;
+for(i = 0; s1[i]; i++)
+;
+for (j = 0; s2[j]; j++)
+;
+s = malloc((i + j + 1) * sizeof(char));
+if (s == 0)
+return (NULL);
+for (i = 0; s1[i]; i++)
+s[i] = s1[i];
+for (j = i; s2[j]; j++)
+s[j] = s2[j - i];
+s[j] = '\0';
+return (s);
+}
