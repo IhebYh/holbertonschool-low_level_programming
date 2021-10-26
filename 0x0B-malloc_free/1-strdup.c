95e5c1fdf3afd5a2ac5dc7bb63db89d73a142b20
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-26 11:06:57 +0100

    2nd commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include<stdlib.h>
+/**
+ * _strdup - entry point
+ * @str:char
+ * Return:Null or pointer
+ */
+char *_strdup(char *str)
+{
+int i = 0, j = 0;
+char *s;
+if (str == NULL)
+return (NULL);
+for (; str[i]; i++)
+j++;
+*s = malloc((j + 1 ) * sizeof(char));
+for (i = 0; i < j ; i++)
+s[i] = str[i];
+s[j] = '\0';
+return (s);
+}
