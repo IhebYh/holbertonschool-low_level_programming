5ad8b7ad6a582b89ade4ce8a7d28193d902c4de5
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-26 10:47:59 +0100

    1st commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include<stdlib.h>
+/**
+ * create_array - entry point
+ * @size: int
+ * @c:char
+ * Return:Null or pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+int i;
+char *allocated_size;
+if (size == 0)
+return (NULL);
+allocated_size = malloc(size * sizeof(char));
+if (allocated_size == 0)
+return (NULL);
+else
+for (i = 0; i < size; i++)
+allocated_size[i] = c;
+return (allocated_size);
+}
