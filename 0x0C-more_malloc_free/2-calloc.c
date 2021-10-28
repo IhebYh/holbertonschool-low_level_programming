e2e1bf39e901f0f949fffc5f4e69801c36ee0fec
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-28 13:39:23 +0100

    3rd commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - entry point
+ * @nmemb:int
+ * @size:int
+ * Return:void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i;
+char *s;
+if (nmemb * size == 0)
+return (NULL);
+s = malloc(nmnemb * size);
+if (s == NULL)
+return (NULL);
+for (i = 0; i < nmemb * size; i++)
+s[i] = '\0';
+return (s);  
+}
