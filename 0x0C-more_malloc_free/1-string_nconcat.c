0db1fc1e9bcfe1f6a9200d489148a9466b303871
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-10-28 13:17:07 +0100

    2nd commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,6 +11,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
 unsigned int i, j, k, l;
+if (n == NULL)
+n = 0;
 for (i = 0; s1[i] != '\0'; i++)
 ;
 for (j = 0; s2[j] != '\0'; j++)
