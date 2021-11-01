9ef838b535220ad632c9c90b44192d1bc7f6f487
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-01 12:21:39 +0100

    Hot fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,13 +14,11 @@ if (d->name == NULL)
 printf("Name: (nil)\n");
 else
 printf("Name: %s\n", d->name);
-if (d->age == NULL)
-printf("Age: (nil)\n");
-else
 printf("Age: %f\n", d->age);
 if (d->owner == NULL)
 printf("Owner: (nil)\n");
 else
 printf("Owner: %s\n", d->owner);
 }
+return ;
 }
