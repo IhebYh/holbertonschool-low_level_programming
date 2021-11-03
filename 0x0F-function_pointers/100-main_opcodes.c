4e763161cc03e1ca119c5a87d126facaef0eadf4
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-03 11:15:26 +0100

    operation code

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main- entry point
+ * @argv:char
+ * @argc:int
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+int i = 0;
+unsigned char *p;
+p = (unsigned char *) main;
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+if (atoi(argv[1]) < 0)
+{
+printf("Error\n");
+exit(2);
+}
+if (atoi(argv[1]) > 0)
+{
+while ( i < (atoi(argv[1]) - 1))
+printf("%02hhx", p[i++]);
+printf("%02hhx\n",p[i]);
+}
+return (0);
+}
