99c5387224703819b486782ee604504e010cf74d
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-03 11:00:34 +0100

    main func

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - entry point
+ * @argv:char
+ * @argc:int
+ * Return:void
+ */
+int main(int argc, char *argv)
+{
+int x, y, res;
+int (*test)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+x = atoi(argv[1]);
+y = atoi(argv[3]);
+test = get_op_func(argv[2]);
+if (fun == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+res = test(x, y);
+printf("%d\n", c);
+return (0);
+}
