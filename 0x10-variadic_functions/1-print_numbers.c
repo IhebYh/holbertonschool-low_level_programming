6f53b74561a34a02d62fc7db2401476e146d7e44
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-05 13:31:54 +0100

    print variadic numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,17 +7,17 @@
  * @separator:char
  * Return:int
  */
-void print_numbers(const char* separator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 unsigned int i;
 va_list l;
 va_start(l, n);
 for (i = 0; i < n; i++)
 {
-printf("%d",va_arg(l, int));
+printf("%d", va_arg(l, int));
 if (separator != NULL && i != n - 1)
 {
-printf("%s",separator);
+printf("%s", separator);
 }
 }
 printf("\n");
