47e104e3c01804ef6fb1515eaa788561f89f81fc
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-12 00:19:17 +0100

    hot fixxxxxx

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 /**
 * print_list - printing linked lists
 * @h: list_t
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
 #ifndef LIST_H
 #define LIST_H
+#include <stdio.h>
 #include <stddef.h>
-
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -16,7 +18,6 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
-_putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
