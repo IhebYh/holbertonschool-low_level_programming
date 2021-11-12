e10fa244b0d54fcd8c5537faa3e3199f46f1627a
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-12 01:29:13 +0100

    betty ffs

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void tortoise (void) __attribute__ ((constructor));
+void tortoise(void) __attribute__ ((constructor));
 /**
 * tortoise - a function that works before main
 * Return : void
 */
 
-void tortoise (void)
+void tortoise(void)
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
