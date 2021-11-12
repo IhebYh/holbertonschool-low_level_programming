d6aead453f4dd4be2667400ef6433fbacb49db82
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-12 01:27:38 +0100

    printing a msg before the main func

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
-#include "main.h"
+#include <stdio.h>
+
+void tortoise (void) __attribute__ ((constructor));
 /**
 * tortoise - a function that works before main
 * Return : void
 */
 
-void tortoise (void) __attribute__ ((constructor));
-
 void tortoise (void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
