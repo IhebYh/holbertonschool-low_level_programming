c340ac756b954be33565f2788ddfc048669311ef
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-16 00:41:05 +0100

    free linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* free_listint2 - free a list
+* @head:list_tint
+* Return:void
+*/
+void free_listint2(listint_t *head)
+{
+listint_t *n;
+if (head == NULL)
+return;
+while (head)
+{
+n = *head;
+n = (*head)->next;
+free(n);
+}
+head = NULL;
+}
