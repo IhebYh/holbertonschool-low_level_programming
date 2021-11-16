d54fb8ed397aa6e2d0d871600775042269860059
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-16 14:28:48 +0100

    hot fixxxxx

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 /**
-* get_nodeint_at - get a node at a given index
+* get_nodeint_at_index - get a node at a given index
 * @head:listint_t
 * @index:int
 * Return:int
 */
-int *get_nodeint_at_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 size_t i = 0;
 while (i < index && head->next)
