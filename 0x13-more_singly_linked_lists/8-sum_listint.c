cacd3e2f50afe457a5ffb474c3bc95feda8ed3e6
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-16 14:36:06 +0100

    hot fix

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -9,11 +9,11 @@ int sum_listint(listint_t *head)
 int sum = 0;
 if (head== NULL)
 return (0);
-while (head ->next)
+while (head->next)
 {
 sum += head->n;
 head = head->next;
 }
-sum += head->n
+sum += head->n;
 return (sum);
 }
