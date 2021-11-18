e6dd9742658bcca97f88264c43e1cf17ffe26976
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-18 11:57:35 +0100

    get bit at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_bit - get bit at a given index
+ * @n: long int
+ * @index: long int
+ * Return: int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (sizeof(n) * 8 != index)
+return (-1);
+n >>= index
+if ((n & 1) == 1)
+return (1);
+else
+return (0);
+}
