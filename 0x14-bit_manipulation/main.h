6d488acdbdd6f88f634a1d076bea1ff040ba4ace
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-18 12:17:26 +0100

    hot fix

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,8 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
