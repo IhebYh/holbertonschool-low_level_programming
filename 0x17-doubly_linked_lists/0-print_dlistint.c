71b0cde826054bc01c990b05c4d3761ca6a8a077
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-06 11:18:02 +0100

    repo init

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a double linked list
+ * @h: dlistint_t
+ * Return: size_t
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t s = 0;
+
+	while (h->next)
+		s++;
+
+	return (s);
+}
