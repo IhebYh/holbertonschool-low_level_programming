a9df2f749e0ab0b68be7e80b13a0baf9f6d8e73e
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-28 13:49:06 +0100

    bugs fixed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,12 +12,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int i, size;
 	hash_node_t *node, *tmp;
 
-	if (!key || !value || !ht)
-		return (0);
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0 || value == NULL)
+			return (0);
 	size = ht->size;
 	i = key_index((const unsigned char *)key, size);
 	tmp = ht->array[i];
-	while (tmp)
+	while (tmp != NULL)
 	{
 		if (tmp && strcmp(key, tmp->key) == 0)
 		{
@@ -27,24 +28,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				return (0);
 			return (1);
 		}
+		tmp = tmp->next;
 	}
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
 	node->key = strdup(key);
 	if (node->key == NULL)
-    {
-        free(node->key);
-        free(node);
-    	return (0);
-    }
+	{
+		free(node->key);
+		free(node);
+		return (0);
+	}
 	node->value = strdup(value);
 	if (node->value == NULL)
-    {
-        free(node->value);
-        free(node);
-    	return (0);
-    }
+	{
+		free(node->value);
+		free(node);
+		return (0);
+	}
 	node->next = ht->array[i];
 	ht->array[i] = node;
 	return (1);
