8328bb641f88c84d26b29e73b2de271a2efd0516
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-28 13:29:39 +0100

    fixed

--- 0x1A-hash_tables/0-hash_tables_create.c
+++ 0x1A-hash_tables/0-hash_tables_create.c
@@ -14,8 +14,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (h_table == NULL)
 		return (NULL);
 	h_table->size = size;
-	h_table->array = malloc(sizeof(h_table->array) * size);
+	h_table->array = malloc(sizeof(hash_node_t) * size);
 	if (h_table->array == NULL)
+	{
+		free(h_table);
 		return (NULL);
+	}
 	return (h_table);
 }
