146aa6c1e273c0cd13025f816cc974bf3c9a30e3
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-28 14:12:39 +0100

    hash table printing

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - a function that prints a hash table
+ * @ht: hash table to print
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *tmp;
+	char flag = 0; /* printing style handler */
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
+	for (; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			if (flag)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			flag = 1;
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
+}
